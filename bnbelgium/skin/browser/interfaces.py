6b4ae069f521b990a94e19d8db8def9276c581d5
Alain Meurant <alain.meurant@affinitic.be>
2009-08-27 12:14:20 +0000

    retrait le moteur de recherche et les portlets promo de belowContent de mainTemplate     creation d'une homePage     creation d'un viewletManager     ajout du viewletManager dans homePage     ajout des portlet promo et des viewlets moteur et carte

--- bnbelgium/skin/browser/interfaces.py
+++ bnbelgium/skin/browser/interfaces.py
@@ -1,7 +1,14 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 from zope.interface import Interface
 from plone.portlets.interfaces import IPortletManager
+from zope.viewlet.interfaces import IViewletManager
 
+
+class IPromoOutil(IViewletManager):
+    """
+    Viewelet manager qui gere les viewlets promo de la homepage
+    sejour fute - idee sejour - boutique - derniere minute
+    """
 
 class IBNBelgiumTheme(IDefaultPloneLayer):
     """
