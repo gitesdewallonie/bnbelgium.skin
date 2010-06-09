54bce49ab98a512e736eff24fdf2cfbe5c8d0fb8
Laurent Lasudry <lasudry@gmail.com>
2010-06-09 13:44:20 +0000

    Display only active 'chambres' in sejour-fute and idee-sejour listings This refs #1674

--- bnbelgium/skin/browser/ideesejour.py
+++ bnbelgium/skin/browser/ideesejour.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-gites.core override
+gites.core
 
 Licensed under the GPL license, see LICENCE.txt for more details.
 Copyright by Affinitic sprl
+
+$Id: event.py 67630 2006-04-27 00:54:03Z jfroche $
 """
 from five import grok
-from gites.core.content.interfaces import ISejourFute
+from gites.core.content.interfaces import IIdeeSejour
 from zope.interface import Interface
 from z3c.sqlalchemy import getSAWrapper
 from bnbelgium.skin.browser.interfaces import IBNBelgiumTheme
@@ -18,39 +20,28 @@ grok.templatedir('templates')
 grok.layer(IBNBelgiumTheme)
 
 
-class SejourFute(grok.View):
+class IdeeSejour(grok.View):
     """
     View on Idee Sejour
     """
-    grok.context(ISejourFute)
-    grok.name('sejour_fute_view')
+    grok.context(IIdeeSejour)
+    grok.name('idee_sejour_view')
     grok.require('zope2.View')
 
     def getHebergements(self):
         """
-        Return the concerned hebergements by this Sejour fute
-        = hebergements linked to the Maison du tourism +
-          select hebergements
+        return the list of hebergement available in the current idee sejour
         """
         wrapper = getSAWrapper('gites_wallons')
         Hebergements = wrapper.getMapper('hebergement')
         Proprio = wrapper.getMapper('proprio')
-        MaisonTourisme = wrapper.getMapper('maison_tourisme')
         session = wrapper.session
-
-        hebList = [int(i) for i in self.context.getHebergementsConcernes()]
-        maisonTourismes = [int(i) for i in self.context.getMaisonsTourisme()]
+        hebList = [int(i) for i in self.context.getHebergements()]
         query = session.query(Hebergements).join('proprio')
         query = query.filter(Hebergements.heb_pk.in_(hebList))
         query = query.filter(Hebergements.heb_site_public == '1')
         query = query.filter(Proprio.pro_etat == True)
-        hebergements = query.all()
-        for maisonTourisme in maisonTourismes:
-            maison = session.query(MaisonTourisme).get(maisonTourisme)
-            for commune in maison.commune:
-                hebergements += list(commune.relatedHebergement)
-        # unique !
-        hebergements = list(set(hebergements))
+        hebergements = list(set(query))
         hebergements.sort(lambda x, y: cmp(x.heb_nom, y.heb_nom))
         hebergements = [hebergement.__of__(self.context.hebergement) \
                         for hebergement in hebergements \
