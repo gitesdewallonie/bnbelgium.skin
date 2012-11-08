1ce939f1eb5406452f71b8dc9f5bdb34130c7a01
Jean-François Roche <jfroche@affinitic.be>
2012-11-08 13:45:50 +0100

    fix imports

--- bnbelgium/skin/browser/vocabulary.py
+++ bnbelgium/skin/browser/vocabulary.py
@@ -2,7 +2,7 @@
 
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.interface import implements
-from zope.app.schema.vocabulary import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyFactory
 from zope.component import queryMultiAdapter
 
 
